be80f3b27d1ad309e26e762358e347345ec04f6b
SteveCordero <stevecords2008@gmail.com>
2021-11-10 08:41:16 -0800

    Added function for each of the nine buttons, to put input into the 2D gridArray

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     //This is the 2D Array that will hold the values of the tic-tac-toe board
@@ -13,4 +14,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void topLeftClick(View v)
+    {
+        gridArray[0][0] = 1;
+    }//end function for the Button 1
+
+    public void topMiddleClick(View v)
+    {
+        gridArray[0][1] = 1;
+    }//end function for the Button 2
+
+    public void topRightClick(View v)
+    {
+        gridArray[0][2] = 1;
+    }//end function for the Button 3
+
+    public void middleLeftClick(View v)
+    {
+        gridArray[0][0] = 1;
+    }//end function for the Button 4
+
+    public void middleMiddleClick(View v)
+    {
+        gridArray[0][0] = 1;
+    }//end function for the Button 5
+
+    public void middleRightClick(View v)
+    {
+        gridArray[0][0] = 1;
+    }//end function for the Button 6
+
+    public void bottomLeftClick(View v)
+    {
+        gridArray[0][0] = 1;
+    }//end function for the Button 7
+
+    public void bottomMiddleClick(View v)
+    {
+        gridArray[0][0] = 1;
+    }//end function for the Button 8
+
+    public void bottomRightClick(View v)
+    {
+        gridArray[0][0] = 1;
+    }//end function for the Button 9
 }
