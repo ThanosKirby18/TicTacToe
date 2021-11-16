0f3c7fc365533713692a62b798376a8fd9850154
SteveCordero <stevecords2008@gmail.com>
2021-11-16 08:25:16 -0800

    Filled 2D array with the value of 2 to represent an empty board

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //This is the 2D Array that will hold the values of the tic-tac-toe board
-    int [][]gridArray = new int[3][3];
+    private int [][]gridArray = fullGrid();
     int playerTurn = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void topLeftClick(View v)
     {
-        gridArray[0][0] = 1;
         Toast.makeText(this, "you clicked button 1", Toast.LENGTH_SHORT).show();
+        gridArray[0][0] = 1;
+
     }//end function for the Button 1
 
     public void topMiddleClick(View v)
@@ -69,4 +70,20 @@ public class MainActivity extends AppCompatActivity {
         gridArray[2][2] = 1;
         Toast.makeText(this, "you clicked button 9", Toast.LENGTH_SHORT).show();
     }//end function for the Button 9
+
+    private int[][] fullGrid()
+    {
+        int[][]grid = new int[3][3];
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                grid[i][j] = 2;
+            }
+        }
+        return grid;
+    }//method to fill up the entire gridArray with the value of 2 to represent an empty board
+
+
+
 }
